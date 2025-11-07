429425f88c14fbe338cd5822cc156387231697d7
Manuel <manuelarvai@gmail.com>
2025-11-07 22:18:18 +0100

    balance

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import { useState } from "react";
 const initialFriends = [
   {
     id: 118836,
-    name: "Clark",
+    name: "Muter",
     image: "https://i.pravatar.cc/48?u=118836",
     balance: -7,
   },
   {
     id: 933372,
-    name: "Sarah",
+    name: "Mama",
     image: "https://i.pravatar.cc/48?u=933372",
     balance: 20,
   },
   {
     id: 499476,
-    name: "Anthony",
+    name: "Babika",
     image: "https://i.pravatar.cc/48?u=499476",
     balance: 0,
   },
@@ -113,12 +113,12 @@ function Friend({ friend, onSelection, selectedFriend }) {
       <h3>{friend.name}</h3>
       {friend.balance < 0 && (
         <p className="red">
-          You owe {friend.name} {friend.balance}€
+          You owe {friend.name} {Math.abs(friend.balance)} Ft.
         </p>
       )}
       {friend.balance > 0 && (
         <p className="green">
-          {friend.name} owes you {Math.abs(friend.balance)}€
+          {friend.name} owes you {friend.balance} Ft.
         </p>
       )}
       {friend.balance === 0 && <p>You and {friend.name} are even</p>}
