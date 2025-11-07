f9459a8875b3af611c72550ccd6a1908a82afc32
Manuel <manuelarvai@gmail.com>
2025-11-07 19:33:54 +0100

    can

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ function Friend({ friend }) {
       <h3>{friend.name}</h3>
       {friend.balance < 0 && (
         <p className="red">
-          You owe {friend.name} {friend.balance}€
+          You can owe {friend.name} {friend.balance}€
         </p>
       )}
       {friend.balance > 0 && (
